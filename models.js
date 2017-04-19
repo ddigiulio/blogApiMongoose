b36fc482932e3be6892425d93e526795343860f1
ddigiulio <ddigiulio90@gmail.com>
2017-04-18 21:29:08 -0700

    updated models

--- models.js
+++ models.js
@@ -1,56 +1,37 @@
 const mongoose = require('mongoose');
 
-// this is our schema to represent a restaurant
-const restaurantSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  borough: {type: String, required: true},
-  cuisine: {type: String, required: true},
-  address: {
-    building: String,
-    // coord will be an array of string values
-    coord: [String],
-    street: String,
-    zipcode: String
-  },
-  // grades will be an array of objects
-  grades: [{
-    date: Date,
-    grade: String,
-    score: Number
-  }]
+// this is our schema to represent a blog
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {type: String, required: true},
+  created: {type: String}
 });
 
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
 // allow us to define properties on our object that manipulate
 // properties that are stored in the database. Here we use it
-// to generate a human readable string based on the address object
+// to generate a human readable string based on the author object
 // we're storing in Mongo.
-restaurantSchema.virtual('addressString').get(function() {
-  return `${this.address.building} ${this.address.street}`.trim()});
+blogPostSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} + ${this.author.lastName}`.trim()});
 
-// this virtual grabs the most recent grade for a restaurant.
-restaurantSchema.virtual('grade').get(function() {
-  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date})[0] || {};
-  return gradeObj.grade;
-});
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
-restaurantSchema.methods.apiRepr = function() {
+blogPostSchema.methods.apiRepr = function() {
 
   return {
-    id: this._id,
-    name: this.name,
-    cuisine: this.cuisine,
-    borough: this.borough,
-    grade: this.grade,
-    address: this.addressString
+    title: this.title,
+    content: this.content,
+    author: this.authorString,
+    created: this.created
   };
 }
 
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const blogPost = mongoose.model('blogPost', blogPostSchema);
 
-module.exports = {Restaurant};
+module.exports = {blogPost};
