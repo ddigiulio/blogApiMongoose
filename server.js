5ddbce8fee1fe0d5ce7f1e30afc8b15d5b38ca1b
ddigiulio <ddigiulio90@gmail.com>
2017-04-18 21:48:20 -0700

    updated server.js

--- server.js
+++ server.js
@@ -2,35 +2,23 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
 const {PORT, DATABASE_URL} = require('./config');
-const {Restaurant} = require('./models');
+const {blogPost} = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
 
-
-// GET requests to /restaurants => return 10 restaurants
-app.get('/restaurants', (req, res) => {
-  Restaurant
-    .find()
-    // we're limiting because restaurants db has > 25,000
-    // documents, and that's too much to process/return
-    .limit(10)
-    // `exec` returns a promise
+app.get('/posts', (req, res) => {
+  blogPost
+    .find()   
+    .limit()
     .exec()
-    // success callback: for each restaurant we got back, we'll
-    // call the `.apiRepr` instance method we've created in
-    // models.js in order to only expose the data we want the API return.
-    .then(restaurants => {
+    .then(blogpost => {
       res.json({
-        restaurants: restaurants.map(
-          (restaurant) => restaurant.apiRepr())
+        blogpost: blogpost.map(
+          (blogpost) => blogpost.apiRepr())
       });
     })
     .catch(
@@ -40,14 +28,11 @@ app.get('/restaurants', (req, res) => {
     });
 });
 
-// can also request by ID
-app.get('/restaurants/:id', (req, res) => {
-  Restaurant
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
+app.get('/posts/:id', (req, res) => {
+  blogPost
     .findById(req.params.id)
     .exec()
-    .then(restaurant =>res.json(restaurant.apiRepr()))
+    .then(blogpost =>res.json(blogpost.apiRepr()))
     .catch(err => {
       console.error(err);
         res.status(500).json({message: 'Internal server error'})
@@ -55,9 +40,9 @@ app.get('/restaurants/:id', (req, res) => {
 });
 
 
-app.post('/restaurants', (req, res) => {
+app.post('/posts', (req, res) => {
 
-  const requiredFields = ['name', 'borough', 'cuisine'];
+  const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -67,15 +52,13 @@ app.post('/restaurants', (req, res) => {
     }
   }
 
-  Restaurant
+  blogPost
     .create({
-      name: req.body.name,
-      borough: req.body.borough,
-      cuisine: req.body.cuisine,
-      grades: req.body.grades,
-      address: req.body.address})
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author})
     .then(
-      restaurant => res.status(201).json(restaurant.apiRepr()))
+      blogpost => res.status(201).json(blogpost.apiRepr()))
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Internal server error'});
@@ -83,8 +66,7 @@ app.post('/restaurants', (req, res) => {
 });
 
 
-app.put('/restaurants/:id', (req, res) => {
-  // ensure that the id in the request path and the one in request body match
+app.put('/posts/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
       `Request path id (${req.params.id}) and request body id ` +
@@ -93,11 +75,8 @@ app.put('/restaurants/:id', (req, res) => {
     res.status(400).json({message: message});
   }
 
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
   const toUpdate = {};
-  const updateableFields = ['name', 'borough', 'cuisine', 'address'];
+  const updateableFields = ['title', 'content', 'author'];
 
   updateableFields.forEach(field => {
     if (field in req.body) {
@@ -105,30 +84,25 @@ app.put('/restaurants/:id', (req, res) => {
     }
   });
 
-  Restaurant
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+  blogPost
     .findByIdAndUpdate(req.params.id, {$set: toUpdate})
     .exec()
-    .then(restaurant => res.status(204).end())
+    .then(blogpost => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
-app.delete('/restaurants/:id', (req, res) => {
-  Restaurant
+app.delete('/posts/:id', (req, res) => {
+  blogPost
     .findByIdAndRemove(req.params.id)
     .exec()
-    .then(restaurant => res.status(204).end())
+    .then(blogpost => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
 let server;
 
 // this function connects to our database, then starts the server
