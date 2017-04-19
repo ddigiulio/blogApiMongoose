73d0c229c1791d66fe2ffb89ba94642a41f57a28
ddigiulio <ddigiulio90@gmail.com>
2017-04-18 21:55:51 -0700

    updated config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blogPosts-app';
+                      'mongodb://localhost/blogPosts';
 exports.PORT = process.env.PORT || 8080;
